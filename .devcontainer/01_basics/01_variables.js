2994a03695f8d47c2243d0eb67ab4daa369de1f9
Harshit Agrawal <agrawalharshit2003@gmail.com>
2026-06-25 20:30:47 +0000

    new

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accountEmail = "agrawalharshit2003@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
 
 // accountId = 2 // not allowed
 
@@ -12,4 +13,9 @@ accountCity = "Bengaluru"
 
 console.log(accountId);
 
-console.table([accountId,accountEmail,accountPassword,accountCity])
+/*
+Prefer not to use var
+because of issue in block scope and functional scope 
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
